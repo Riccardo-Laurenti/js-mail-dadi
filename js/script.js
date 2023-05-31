da9069756f6c586c9bf2c667589bdc566555b3d1
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-05-31 17:30:55 +0200

    finish 1 exercise

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
+// MAIL
+//- Inventiamo una lista di email a nostro piacimento
+//- Chiediamo all'utente una mail (con un prompt)
+//- controlliamo che la mail inserita sia presente nella lista
+//- se è presente, gli diamo il benvenuto. (in console)
+//- altrimenti gli diciamo che non può accedere (in console)
+
+// ---- > Avviso:non potete usare il metodo .includes ())!!!
+
+const recElement = document.getElementById('mail-dadi');
+
+const getEmail = ['riccardo.laurenti95@gmail.com', 'laurachisci@gmail.com', 'maurilaurenti@yahoo.it'];
+console.table(getEmail);
+
+const userEmail = prompt('Inserisci qui la tua email');
+console.log(userEmail);
+
+let isSave = false;
+
+for ( let i = 0; i < getEmail.length; i++ ) {
+    if (userEmail === getEmail[i]) {
+        console.log('BENVENUTO !',`Email riconosciuta`);
+        isSave = true;
+    }
+}
 
 
-
-
+if (isSave) {
+    console.log(userEmail);
+} else {
+    console.log('Email errata, non puoi accedere');
+}
--- steps.md
+++ steps.md
@@ -1,1 +1,15 @@
+## 1 Inventiamo una lista di email a nostro piacimento
 
+## 2 Chiediamo all'utente una mail (con un prompt)
+
+## 3 Controlliamo che la mail inserita sia presente nella lista
+
+## 4 Se è presente, gli diamo il benvenuto. (in console)
+
+## 5 Altrimenti gli diciamo che non può accedere (in console)
+
+---
+
+---
+
+---
